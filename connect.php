67b63cae5fbadaa023144446acbcae973606fd38
Tejaswini Nanaware <35250916+TejaswiniNanaware@users.noreply.github.com>
2018-09-06 23:05:04 +1000

    connect file deleted

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $mysql_hostname="localhost";
-    $mysql_user="root";
-    $mysql_password = "Ver$@ce123";
-    $mysql_database = "drugex";
-    $prefix = "";
-    $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password,$mysql_database) or die("Could not connect database");
- ?>
