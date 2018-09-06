8ce35e0c44c088f18738370d21477a71d84f3da3
TejaswiniNanaware <nanawaretejaswini@gmail.com>
2018-09-06 23:02:10 +1000

    modular nav git ignore and css file committed

---
+++ nav.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Harmful Substance</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+
+        .container {
+            position: relative;
+        }
+
+        .topleft {
+            position: absolute;
+            top: 8px;
+            left: 16px;
+            font-size: 18px;
+        }
+
+        .center {
+            position: absolute;
+            left: 0;
+            top: 50%;
+            width: 100%;
+            text-align: center;
+            font-size: 18px;
+        }
+
+        img {
+            width: 100%;
+            height: auto;
+            opacity: 0.3;
+        }
+
+        h1 {
+            display: block;
+            font-size: 4em;
+            /*margin-top: 0.67em;*/
+            /*margin-bottom: 0.67em;*/
+            /*margin-left: 0;*/
+            /*margin-right: 0;*/
+            font-weight: bold;
+            text-align: center;
+            color:
+
+        }
+
+        /*button {*/
+            /*background-color: darkgrey;*/
+            /*border: none;*/
+            /*color: black;*/
+            /*padding: 14px 40px;*/
+            /*text-align: center;*/
+            /*text-decoration: none;*/
+            /*display: inline-block;*/
+            /*font-size: 16px;*/
+            /*margin: 4px 2px;*/
+            /*cursor: pointer;*/
+        /*}*/
+
+        /*.button2 {*/
+            /*background-color: #4CAF50;*/
+            /*border: none;*/
+            /*color: white;*/
+            /*padding: 15px 32px;*/
+            /*text-align: center;*/
+            /*text-decoration: none;*/
+            /*display: inline-block;*/
+            /*font-size: 16px;*/
+            /*margin: 4px 2px;*/
+            /*cursor: pointer;*/
+        /*}*/
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+            background-color: #111;
+
+        }
+
+        .active {
+            background-color: darkgrey;
+        }
+
+        div.relative {
+            position: relative;
+            left: 30px;
+            /*border: ;*/
+        }
+
+        div.fixed {
+            position: fixed;
+            bottom: 0;
+            right: 0;
+            width: 300px;
+            border: 3px solid #73AD21;
+        }
+
+
+
+    </style>
+</head>
+<body>
+
+<h1>DrugEx</h1>
+
+<ul>
+    <!--<li><a href="#home"> <button type="button">Home</button> </a></li>-->
+    <!--<li><a href="#condition_search"> <button type="button">Search for the nearest support</button> </a></li>-->
+    <!--<li><a href="#quiz"> <button type="button">Quiz</button> </a></li>-->
+    <!--<li style="float:right"><a class="active" href="#about">About Us</a></li>-->
+
+    <li><a href="drugex.html"> Drug Facts </a></li>
+    <li><a href="condition_search.php"> Search for the nearest support </a></li>
+    <li><a href="12-to-17-tobacco-use-percentage.php"> Statistics </a></li>
+    <li><a href="visualizations.php"> Perspective</a></li>
+    <li style="float:right"><a class="active" href="about-us.html">About Us</a></li>
+</ul>
+
+<div class="container">
+    <!--<div class="topleft">-->
+        <!--<img width="100px" height="auto" src="heroin-1.jpg"> <img width="100px" height="auto" src="heroin-2.jpg">-->
+        <!--<p> Heroin is a depressant drug, which means it slows down the messages travelling between your brain and body.-->
+            <!--Heroin belongs to a group of drugs known as opiates that are from the opium poppy</p>-->
+    <!--</div>-->
+
+</div>
